05f43dc702254fbbdd1bca25c91ecf57a09bd03d
jib7 <jib7@cs-ssh.argo.uwf.edu>
2017-03-28 22:43:57 -0500

    Updated menu designs / header file

--- draw.c
+++ draw.c
@@ -3,6 +3,7 @@
 #include <string.h>
 
 #include "login.h"
+#include "draw.h"
 
 /* Commonly used drawing characters
  *
@@ -15,15 +16,15 @@
 void drawLogin()
 {
 	/* clear the screen */
-	//system(clear);
+	system("clear");
 	
 	printf("\n\n");
-	printf("     .---------------------.\n");
-	printf("     |    Health Record    |\n");
-	printf("     |     System 2000     |\n");
-	printf("     |                     |\n");
-	printf("     |        v 1.4        |\n");
-	printf("     '---------------------'\n");
+	printf("     \x1b[32m╔═════════════════════╗▄\x1b[0m\n");
+	printf("     ║    Health Record    ║█\n");
+	printf("     ║     System 2000     ║█\n");
+	printf("     ║        v 1.4        ║█\n");
+	printf("     ╚═════════════════════╝█\n");
+	printf("        ▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n");
 	printf("\n");
 	printf("             Welcome!\n");
 }
@@ -32,54 +33,66 @@ void drawLogin()
 void drawMenu(User currentUser)
 {
 	/* clear the screen */
-	//system(clear);
+	system("clear");
 	
-	printf("\n");
-	printf("---> Hello, %s\n", currentUser->name);
-	printf("\n\n");
-	printf("              .-----------.\n");
-	printf("              | Main Menu |\n");
-	printf("--------------'-----------'------------------\n\n");
-	
+	char *name = userGetName(currentUser);
+	int type = userGetType(currentUser);
+
+		printf("\n\n");
+		printf("╔════════════════ MAIN MENU ═════════════════╗\n");
+		printf("║ Hello, %-35s ║\n", name);
+		printf("╟────────────────────────────────────────────╢\n");
 	/* if doctor/nurse */
-	if(currentUser->type == 0 || currentUser->type == 1)
+	if(type == 0 || type == 1)
 	{
-		printf("     1 |    Search Patients\n");
-		printf("     2 | Create New Patient\n");
-		printf("     ----------------------\n");
-		printf("     4 |    Change Password\n");
-		printf("     5 |            Log Off\n");
+		printf("║         ┌───┬────────────────────┐         ║\n");
+		printf("║         │ 1 │    Search Patients │         ║\n");
+		printf("║         │ 2 │ Create New Patient │         ║\n");
+		printf("║         ├───┼────────────────────┤         ║\n");
+		printf("║         │ 3 │    Change Password │         ║\n");
+		printf("║         │ 4 │            Log Off │         ║\n");
+		printf("║         └───┴────────────────────┘         ║\n");
+		printf("╚════════════════════════════════════════════╝\n");
 		printf("\n");
 	}
 	/* if help desk */
-	else if(currentUser->type == 2)
+	else if(type == 2)
 	{
-		printf("     1 |      View Appointments\n");
-		printf("     2 | Create New Appointment\n");
-		printf("     3 |     Delete Appointment\n");
-		printf("     --------------------------\n");
-		printf("     4 |        Change Password\n");
-		printf("     5 |                Log Off\n");
+		printf("║       ┌───┬────────────────────────┐       ║\n");
+		printf("║       │ 1 │      View Appointments │       ║\n");
+		printf("║       │ 2 │ Create New Appointment │       ║\n");
+		printf("║       │ 3 │     Delete Appointment │       ║\n");
+		printf("║       ├───┼────────────────────────┤       ║\n");
+		printf("║       │ 4 │        Change Password │       ║\n");
+		printf("║       │ 5 │                Log Off │       ║\n");
+		printf("║       └───┴────────────────────────┘       ║\n");
+		printf("╚════════════════════════════════════════════╝\n");
 		printf("\n");
 	}
 	/* if auditor */
-	else if(currentUser->type == 3)
+	else if(type == 3)
 	{
-		printf("     1 | View System Logs\n");
-		printf("     --------------------\n");
-		printf("     2 |  Change Password\n");
-		printf("     3 |             Exit\n");
+		printf("║          ┌───┬──────────────────┐          ║\n");
+		printf("║          │ 1 │ View System Logs │          ║\n");
+		printf("║          ├───┼──────────────────┤          ║\n");
+		printf("║          │ 2 │  Change Password │          ║\n");
+		printf("║          │ 3 │          Log Off │          ║\n");
+		printf("║          └───┴──────────────────┘          ║\n");
+		printf("╚════════════════════════════════════════════╝\n");
 		printf("\n");
 	}
 	/* if system admin */
-	else if(currentUser->type == 4)
+	else if(type == 4)
 	{
-		printf("     1 |      View Users\n");
-		printf("     2 | Create New User\n");
-		printf("     3 |     Delete User\n");
-		printf("     -------------------\n");
-		printf("     4 | Change Password\n");
-		printf("     5 |         Log Off\n");
+		printf("║           ┌───┬─────────────────┐          ║\n");
+		printf("║           │ 1 │      View Users │          ║\n");
+		printf("║           │ 2 │ Create New User │          ║\n");
+		printf("║           │ 3 │     Delete User │          ║\n");
+		printf("║           ├───┼─────────────────┤          ║\n");
+		printf("║           │ 4 │ Change Password │          ║\n");
+		printf("║           │ 5 │         Log Off │          ║\n");
+		printf("║           └───┴─────────────────┘          ║\n");
+		printf("╚════════════════════════════════════════════╝\n");
 		printf("\n");
 	}
 	/* if for some reason invalid type */
@@ -91,12 +104,4 @@ void drawMenu(User currentUser)
 		printf("If you believe this to be in error, please contact the system administrator.\n");
 		printf("\n");
 	}
-}
-
-/* debug driver */
-int main()
-{
-	drawLogin();
-	
-	return 0;
 }
